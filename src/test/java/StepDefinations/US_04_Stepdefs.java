6384dcb23b3e509b64b671e7236ef059c4048622
sareh <srhnmlgnr34@hotmail.com>
2020-12-03 22:24:02 +0100

    steps are created

--- src/test/java/StepDefinations/US_04_Stepdefs.java
+++ src/test/java/StepDefinations/US_04_Stepdefs.java
@@ -1,4 +1,17 @@
 package StepDefinations;
 
+import cucumber.api.java.en.When;
+
 public class US_04_Stepdefs {
+    @When("^User should be able to create a new Discount$")
+    public void userShouldBeAbleToCreateANewDiscount() {
+    }
+
+    @When("^User should be able to update an existant Discount$")
+    public void userShouldBeAbleToUpdateAnExistantDiscount() {
+    }
+
+    @When("^User should be able to delete an existant Discount$")
+    public void userShouldBeAbleToDeleteAnExistantDiscount() {
+    }
 }
