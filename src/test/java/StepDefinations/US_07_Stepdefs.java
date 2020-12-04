b823b4293bcc7f6aafe4ab90ccba9ba5981b08c4
otosun46 <64793578+otosun46@users.noreply.github.com>
2020-12-05 00:16:42 +0100

    Update US_07_Stepdefs.java

--- src/test/java/StepDefinations/US_07_Stepdefs.java
+++ src/test/java/StepDefinations/US_07_Stepdefs.java
@@ -9,23 +9,5 @@ import cucumber.api.java.en.When;
 
 public class US_07_Stepdefs {
 
-    NavbarContent navbarContent=new NavbarContent();
-    US_07_Content us_07_content=new US_07_Content();
-
-    @When("^User should be able to create a new Discount$")
-    public void userShouldBeAbleToCreateANewDiscount() {
-        navbarContent.findElementAndClickFunction("setupOne");
-        navbarContent.findElementAndClickFunction("parameters");
-        navbarContent.findElementAndClickFunction("discounts");
-       // us_07_content.findElementAndClickFunction("addButton");
-
-    }
-
-    @When("^User should be able to update an existant Discount$")
-    public void userShouldBeAbleToUpdateAnExistantDiscount() {
-    }
-
-    @When("^User should be able to delete an existant Discount$")
-    public void userShouldBeAbleToDeleteAnExistantDiscount() {
-    }
+    
 }
