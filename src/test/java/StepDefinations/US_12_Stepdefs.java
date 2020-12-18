59323477f7e088f6abcdcd7b0e19f10ad9b56df3
ferah5534 <hakferah@gmail.com>
2020-12-18 14:22:07 -0700

    US_12 created

---
+++ src/test/java/StepDefinations/US_12_Stepdefs.java
@@ -0,0 +1,20 @@
+package StepDefinations;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class US_12_Stepdefs {
+
+    @When("^User should be able to view Attestations page$")
+    public void userShouldBeAbleToViewAttestationsPage() {
+    }
+
+    @Then("^User  should be able to create an Attestation and view  successfully validating the message$")
+    public void userShouldBeAbleToCreateAnAttestationAndViewSuccessfullyValidatingTheMessage() {
+    }
+    @And("^User should  be able to edit Attestation and view  successfully validating the message$")
+    public void userShouldBeAbleToEditAttestationAndViewSuccessfullyValidatingTheMessage() {
+
+    }
+}
